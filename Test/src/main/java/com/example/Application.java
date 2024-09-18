c2740009f2c25b0de9b4b479a4740de06b0de26c
DevelopersPost <i.rajeev92@gmail.com>
2024-09-18 06:14:41 +0530

    commit

--- Test/src/main/java/com/example/Application.java
+++ Test/src/main/java/com/example/Application.java
@@ -1,34 +1,15 @@
 package com.example;
 
-import com.example.model.Bid;
-import com.example.model.Item;
-import com.example.model.User;
-import com.example.repositories.BidRepository;
-import com.example.repositories.ItemRepository;
-import com.example.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Set;
-
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application {
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    ItemRepository itemRepository;
-
-    @Autowired
-    BidRepository bidRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
@@ -37,26 +18,6 @@ public class Application {
     @Bean
     public CommandLineRunner dbOperation() {
         return x -> {
-
-            User john = new User("John Smith");
-            userRepository.save(john);
-
-            Item item = new Item("Foo");
-
-            Bid bid = new Bid(BigDecimal.valueOf(100), item);
-            Bid bid2 = new Bid(BigDecimal.valueOf(200), item);
-
-            item.addBid(bid);
-            bid.setBidder(john);
-            item.addBid(bid2);
-            bid2.setBidder(john);
-
-            itemRepository.save(item);
-
-            List<Item> items = itemRepository.findAll();
-            Set<Bid> bids = bidRepository.findByItem(item);
-            User user = userRepository.findUserWithBids(john.getId());
-
 
 
         };
--- Test/src/main/java/com/example/repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repositories;
-
-import com.example.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item,Long> {
-}
